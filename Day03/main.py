13990b118d17d70cd2d1d71169190d4826bc0bf1
William Owens <williamlewisowens@gmail.com>
2022-12-03 21:51:52 -0600

    quick cleanup

--- Day03/main.py
+++ Day03/main.py
@@ -9,18 +9,31 @@ def readFile(fileName):
     _input = file.read().splitlines()
   return _input
 
+def calculateRunningTotal(inputSet):
+  partialRunningTotal = 0
+  for j in inputSet:
+    if (ord(j) > 90):
+      partialRunningTotal += ord(j) - ASCII_DIFFERENCE_SMALL
+    else:
+      partialRunningTotal += ord(j) - ASCII_DIFFERENCE_BIG
+  return partialRunningTotal
+
 def solution1(l):
   runningTotal = 0
   for i in range(0, len(l)):
     halfLength = int(len(l[i])/2)
-    unionGroup = set(set(l[i][:halfLength]).intersection(set(l[i][halfLength:])))
-    for j in unionGroup:
-      if (ord(j) > 90):
-        runningTotal += ord(j) - ASCII_DIFFERENCE_SMALL
-      else:
-        runningTotal += ord(j) - ASCII_DIFFERENCE_BIG
+    unionGroup = set(l[i][:halfLength]) & set(l[i][halfLength:])
+    runningTotal += calculateRunningTotal(unionGroup)
   return runningTotal
+
+def solution2(l):
+  runningTotalPartTwo = 0
+  for i in range(0, len(l), 3):
+    setOfAllThree = set(l[i]) & set(l[i+1]) & set(l[i+2])
+    runningTotalPartTwo += calculateRunningTotal(setOfAllThree)
+  return runningTotalPartTwo
 
 if __name__ == "__main__":
     _input = readFile("input.txt")
     print(solution1(_input))
+    print(solution2(_input))
