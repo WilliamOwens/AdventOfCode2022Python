cd7f7e4b1eeab6db2593898ff9bb6aba3ccd2db3
William Owens <williamlewisowens@gmail.com>
2022-12-04 22:25:59 -0600

    cleanup logic

--- Day04/main.py
+++ Day04/main.py
@@ -1,4 +1,5 @@
-import os, re
+import os
+import re
 
 def readFile(fileName):
   dir = os.path.abspath(os.path.dirname(__file__))
@@ -12,25 +13,15 @@ def prepData(d):
 def solution(l, fullOverlap):
   runningTotal = 0
   for line in l:
-    if not fullOverlap:
-      if ((line[0] <= line[3]) & (line[1] >= line[2])):
+    if fullOverlap:
+      if (((line[0] >= line[2]) & (line[1] <= line[3])) | (((line[2] >= line[0]) & (line[3] <= line[1])))):
         runningTotal += 1
     else:
-      firstSection = line[1] - line[0]
-      secondSection = line[3] - line[2]
-      if (firstSection == secondSection):
-        if (line[0] == line[2]):
-          runningTotal += 1
-      else:
-        if firstSection > secondSection:
-          LARGER_GAP, LARGER_START, SMALLER_GAP, SMALLER_START = firstSection, line[0], secondSection, line[2]
-        else:
-          LARGER_GAP, LARGER_START, SMALLER_GAP, SMALLER_START = secondSection, line[2], firstSection, line[0]
-        if ((SMALLER_START >= LARGER_START) & (SMALLER_START <= (LARGER_START + (LARGER_GAP - SMALLER_GAP)))):
-          runningTotal += 1
+      if ((line[0] <= line[3]) & (line[1] >= line[2])):
+        runningTotal += 1
   return runningTotal
 
 if __name__ == "__main__":
-    _input = readFile("input.txt")
-    print(solution(_input, True))
-    print(solution(_input, False))
+  _input = readFile("input.txt")
+  print(solution(_input, True))
+  print(solution(_input, False))
